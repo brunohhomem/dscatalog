3d58c9de6754d3c2432dacb8f26efc954301eab6
Bruno H. Homem <brunohhomem@gmail.com>
2024-01-10 15:59:24 -0300

    creation: Category entity & CategoryResource

---
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+@Table
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
