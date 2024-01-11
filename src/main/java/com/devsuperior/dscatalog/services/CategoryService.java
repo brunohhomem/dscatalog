6c696a7857958e0b4e8390b0a345198a1024e1c6
Bruno H. Homem <brunohhomem@gmail.com>
2024-01-11 16:21:54 -0300

    H2 seeding

--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,13 +2,16 @@ package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service //registra a classe como um componente de injeção de dependencia
 public class CategoryService {
     @Autowired private CategoryRepository categoryRepository;
+    @Transactional(readOnly = true)
     public List<Category> findAll(){return categoryRepository.findAll();}
 }
