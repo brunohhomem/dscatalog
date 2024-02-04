ff0ae48af8671e31e9dab3acaeb3ce4fa682ecaf
Bruno H. Homem <brunohhomem@gmail.com>
2024-02-04 11:06:22 -0300

    Validation error handling

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ValidationError.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import lombok.Getter;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationError extends StandardError{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public void addError(String fieldName, String message){
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
