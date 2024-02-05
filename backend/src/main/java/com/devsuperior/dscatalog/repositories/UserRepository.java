8e4c815e3006c08c649feb26b425e22cd4952143
Bruno H. Homem <brunohhomem@gmail.com>
2024-02-05 18:49:27 -0300

    added: findByEmail

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email); //Buscar usuário usando email
+
+}
